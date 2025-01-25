611fff8f7393fc01cca3976e2b325a30da42266a
pnIndieMac <179102596+pnindie@users.noreply.github.com>
2025-01-25 15:05:58 +0530

    Leap Year solution
    Solution for leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+    let isLeap = false
+    
+    if(inputYear%4 == 0){
+        if(inputYear%100 == 0){
+            if(inputYear%400 == 0){
+                isLeap = true 
+            }
+    
+        }else{
+        isLeap = true
+        }
+    }
+    
+    return isLeap
 
 };
 
