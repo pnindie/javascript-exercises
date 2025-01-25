2c880a4dbbd9cf47bd16f7315d6202566ea15f84
pnIndieMac <179102596+pnindie@users.noreply.github.com>
2025-01-25 12:51:49 +0530

    Solution to third-javascript
    string reversal

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(inputTxt) {
+    let charArray = []
+    let reversedString = ''
+    charArray = inputTxt.split("")
+    for(let i=0; i<inputTxt.length; i++){
+        reversedString = reversedString + charArray[inputTxt.length-1-i]
+    }
+    return reversedString
 };
 
 // Do not edit below this line
