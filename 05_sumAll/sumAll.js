420a6479c774446c073bcbc9637fddc466c165d0
pnIndieMac <179102596+pnindie@users.noreply.github.com>
2025-01-25 13:15:15 +0530

    A clever way to find positive integers
    A clean and neat code to achieve the positive sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+function isPositiveInteger(n) {
+    return  typeof n !== 'string' && n >>> 0 === parseFloat(n);
+}
 
+const sumAll = function(num1, num2) {
+    if(isPositiveInteger(num1) && isPositiveInteger(num2)){
+        let sum=0;
+        for(let i=Math.min(num1,num2); i<=Math.max(num1,num2);i++){
+            sum +=i;
+        }
+        return sum
+    }else{
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
