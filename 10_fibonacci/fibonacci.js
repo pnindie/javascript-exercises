0598f26f062fa14a8e81ec4554b7d393948ec0c2
pnIndieMac <179102596+pnindie@users.noreply.github.com>
2025-01-26 18:06:50 +0530

    fibonacci - javascript
    It was easy one

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(inputValue) {
+    let index = parseInt(inputValue)
+    if(index<0){
+        return "OOPS"
+    }else{
+    
+    let fibArr = [0, 1, 1]
+    
+    if(index>2){
+        for(let i=3; i<=index;i++){
+            fibArr.push(fibArr[i-1]+fibArr[i-2])
+        }
+        
+    }
+
+    return fibArr[index]
+    }
 
 };
 
