7797d4fffeb0dfe23e531462067613188211d02d
pnIndieMac <179102596+pnindie@users.noreply.github.com>
2025-01-27 15:53:10 +0530

    solution to age-javascript
    js array methods are very powerful and can do great amount of work in feew lines

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear()
 
+    let agesArray = people.map((person) => 
+        person.hasOwnProperty('yearOfDeath') ?
+            (person.age = person.yearOfDeath-person.yearOfBirth)
+        :
+            (person.age = currentYear-person.yearOfBirth)
+        
+    )
+    let indexOfMaxValue = agesArray.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+    console.log(agesArray,indexOfMaxValue,people[indexOfMaxValue],people[indexOfMaxValue]["name"])
+    return  people[indexOfMaxValue]
+    
 };
 
 // Do not edit below this line
